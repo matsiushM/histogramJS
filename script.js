de8104b5f139e3aa6295241c09b9f26557571163
Максим <matiush2018@yandex.ru>
2023-05-22 18:21:49 +0300

    add flex,add filter numbers

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 const table = document.getElementById('container');
-const inputElement = document.getElementById("input-id");
+const inputElement = document.getElementById("input");
 const inputElementEnter = document.getElementById('input-button')
 
 
 const drawHistogram = () => {
-    const numArray = inputElement.value.trim().split(" ");
+    table.innerHTML = "";
 
-    const item = Array.from({length: numArray.length}, (_, index) => ({
-            id: index,
-            element: document.createElement('div')
-    }));
+    const numArray = inputElement.value.trim().split(" ").filter(Number);
+    const maxNum = Math.max(...numArray);
 
-    item.forEach((e) => {
-        table.insertAdjacentElement('beforeend', e.element)
+    numArray.forEach((num) => {
+        // table.insertAdjacentElement('beforeend', element)
 
-        const barHeight = (10 * numArray[e.id]);
+        const element = document.createElement("div");
+        const barHeight = ((num/maxNum) * 100);
 
-        e.element.innerHTML = numArray[e.id];
-        e.element.classList.add('barElement');
-        e.element.style.height = barHeight + "px";
+        element.innerHTML = num;
+        element.classList.add('barElement');
+        element.style.height = barHeight + "%";
+        table.append(element)
     })
+    inputElement.value = '';
 };
 
 inputElementEnter.addEventListener("click", drawHistogram);
