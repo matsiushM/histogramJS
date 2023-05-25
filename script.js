5c2cd6829ab6840b02baa7ce088fc451345bff60
Максим <matiush2018@yandex.ru>
2023-05-25 13:49:00 +0300

    fix create array

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-const table = document.getElementById('container');
-const inputElement = document.getElementById("input");
-const buttonInput = document.getElementById('button-input');
-const buttonSort = document.getElementById("button-sort");
-const buttonSortBubble = document.getElementById('button-sort_bubble');
+const histogram = () => {
+    const table = document.getElementById('container');
+    const inputElement = document.getElementById("input");
+    const buttonInput = document.getElementById('button-input');
+    const buttonSort = document.getElementById("button-sort");
+    const buttonSortBubble = document.getElementById('button-sort_bubble');
 
-const drawHistogram = (numArray) => {
-    table.innerHTML = "";
-    const maxNum = Math.max(...numArray);
+    let numArray = [];
+    const drawHistogram = () => {
+        table.innerHTML = "";
+        const maxNum = Math.max(...numArray);
 
-    numArray.forEach((num) => {
+        numArray.forEach((num) => {
 
-        const element = document.createElement("div");
-        const barHeight = ((num / maxNum) * 100);
+            const element = document.createElement("div");
+            const barHeight = ((num / maxNum) * 100);
 
-        element.innerText = num;
-        element.classList.add('barElement');
-        element.style.height = barHeight + "%";
-        table.append(element)
-    })
+            element.innerText = num;
+            element.classList.add('barElement');
+            element.style.height = barHeight + "%";
+            table.append(element)
+        })
 
-}
-const getArray = () => {
-    return inputElement.value.trim().split(" ").filter(Number).map(Number);
-}
+    }
 
-const drawDefault = () => {
-    const numArray = getArray();
-    drawHistogram(numArray);
-}
+    const drawDefault = () => {
+        numArray = inputElement.value.trim().split(" ").filter(Number).map(Number);
+        drawHistogram();
+    }
 
-const sortNums = () => {
-    const numArray = getArray();
+    const sortNums = () => {
+        numArray.sort((a, b) => a - b);
+        drawHistogram();
+    }
 
-    numArray.sort((a, b) => a - b);
-    drawHistogram(numArray);
-}
-
-const sortNumsBubble = () => {
-    const numArray = getArray();
-
-    for (let i = 0; i < numArray.length; i++) {
-        for (let j = numArray.length - 1; j > i; j--) {
-            if (numArray[j] > numArray[j - 1]) {
-                const num = numArray[j];
-                numArray[j] = numArray[j - 1];
-                numArray[j - 1] = num;
+    const sortNumsBubble = () => {
+        for (let i = 0; i < numArray.length; i++) {
+            for (let j = numArray.length - 1; j > i; j--) {
+                if (numArray[j] > numArray[j - 1]) {
+                    const num = numArray[j];
+                    numArray[j] = numArray[j - 1];
+                    numArray[j - 1] = num;
+                }
             }
         }
+        drawHistogram()
     }
-    drawHistogram(numArray)
+
+    buttonSortBubble.addEventListener("click", sortNumsBubble)
+    buttonSort.addEventListener("click", sortNums);
+    buttonInput.addEventListener("click", drawDefault);
 }
 
-buttonSortBubble.addEventListener("click", sortNumsBubble)
-buttonSort.addEventListener("click", sortNums);
-buttonInput.addEventListener("click", drawDefault);
+histogram();
 
 
