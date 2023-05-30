ad7eaf025e63c46adec0f8849b3c6f35f3e08d05
Максим <matiush2018@yandex.ru>
2023-05-30 19:49:28 +0300

    fixed animation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const histogram = () => {
     const histogramArea = document.getElementById('histogramArea');
     const inputElement = document.getElementById('input');
@@ -6,21 +5,30 @@ const histogram = () => {
     const buttonPanel = document.getElementById('buttonPanel');
     const sortButton = document.getElementById('sortButton');
     const sortButtonOrder = document.getElementById('sortButtonOrder');
-    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    let isStopped = false;
     const sortNumsBubble = async () => {
-        const nodeArray = histogramArea.childNodes;
-            for (let i = 0; i < nodeArray.length; i++) {
-                for (let j = 0; j < nodeArray.length-1-i; j++) {
-                    if (Number(nodeArray[j].textContent) > Number(nodeArray[j + 1].textContent)) {
-                        nodeArray[j + 1].after(nodeArray[j]);
-                    }
-                   await delay(200);
+        isStopped = false;
+       const nodeArray = histogramArea.childNodes;
+        for (let i = 0; i < nodeArray.length; i++) {
+            for (let j = 0; j < nodeArray.length - 1 - i; j++) {
+                if (Number(nodeArray[j].textContent) > Number(nodeArray[j + 1].textContent)) {
+                    nodeArray[j + 1].after(nodeArray[j]);
+                }
+                await delay(200);
+
+                if (isStopped) {
+                    break; // Выход из функции
                 }
             }
+            if (isStopped) {
+                break; // Выход из функции
+            }
+        }
     }
 
-    const sortNums= () => {
+    const sortNums = () => {
         const nodeArray = histogramArea.childNodes;
         for (let i = 0; i < nodeArray.length; i++) {
             nodeArray[i].style.order = Number(nodeArray[i].textContent);
@@ -42,6 +50,7 @@ const histogram = () => {
     }
 
     const showHistogram = () => {
+        isStopped = true;
         const numArray = inputElement.value.trim().split(" ").filter(Number).map(Number);
         if (numArray.length === 0) {
             histogramArea.innerHTML = "";
@@ -55,7 +64,7 @@ const histogram = () => {
     }
 
     enterButton.addEventListener("click", showHistogram);
-    sortButton.addEventListener("click",sortNumsBubble)
+    sortButton.addEventListener("click", sortNumsBubble)
     sortButtonOrder.addEventListener("click", sortNums)
 }
 
